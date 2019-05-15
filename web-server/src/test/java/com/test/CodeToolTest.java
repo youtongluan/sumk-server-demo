0adb75ac489354b2114ec2e0afb38ffa4c8acd84
youtl <youtl@163.com>
2019-05-15 18:33:08 +0800

    更新

---
+++ web-server/src/test/java/com/test/CodeToolTest.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import org.junit.Test;
+import org.test.pojo.Student;
+import org.yx.sumk.code.CodeTool;
+
+public class CodeToolTest {
+
+	//要在bean生成之后
+	@Test
+	public void generateDao(){
+		CodeTool.generateDao(Student.class);
+	}
+	
+	@Test
+	public void generateDBTable(){
+		CodeTool.generateDBTable();
+	}
+
+}
