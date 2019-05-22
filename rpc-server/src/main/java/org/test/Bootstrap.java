bf4a90c1dc4661083a6f2a3b8cc1e835dc359739
youtl <youtl@163.com>
2019-05-22 20:28:43 +0800

    更新sumk到2.3.3

--- rpc-server/src/main/java/org/test/Bootstrap.java
+++ rpc-server/src/main/java/org/test/Bootstrap.java
@@ -1,28 +1,11 @@
 package org.test;
 
-import org.yx.conf.AppInfo;
-import org.yx.log.Log;
 import org.yx.main.SumkServer;
 
 public class Bootstrap {
 
 	public static void main(String[] args) {
-		try {
-			long begin=System.currentTimeMillis();
-			SumkServer.start();
-			Log.get(Bootstrap.class).info("启动完成,耗时：{}毫秒",System.currentTimeMillis()-begin);
-
-			for(;;){
-				if(AppInfo.getBoolean("stopServer", false)){
-					SumkServer.stop();
-					System.exit(-1);
-				}
-				Thread.sleep(1000);
-			}
-		} catch (Exception e) {
-			Log.printStack(e);
-		}
-
+		SumkServer.main(args);
 	}
 
 }
