f7a29efcd33786213ae36d984383aec9f9367dff
youtl <3205207767@qq.com>
2020-11-18 21:03:31 +0800

    293

--- web-server/src/main/java/org/test/web/UploadAction.java
+++ web-server/src/main/java/org/test/web/UploadAction.java
@@ -1,8 +1,14 @@
 package org.test.web;
 
+import java.io.IOException;
+
 import org.yx.annotation.Bean;
 import org.yx.annotation.http.Upload;
 import org.yx.annotation.http.Web;
+import org.yx.conf.AppInfo;
+import org.yx.http.handler.MultipartItem;
+import org.yx.util.StreamUtil;
+import org.yx.util.WebUtil;
 
 @Bean
 public class UploadAction {
@@ -10,13 +16,20 @@ public class UploadAction {
 	/*
 	 * 添加Upload就表示支持文件上传。它的类型必须是multipart/form-data。
 	 * 其中名为param的part会被解析成参数。
-	 * 其它的需要通过WebUtil来获取
+	 * 其它的需要通过WebUtil来获取.
+	 * aa.postman_collection.json文件是postman的测试文件
+	 * http://localhost:8081/upload/uploadFile
 	 */
 	@Web
 	@Upload
-	public void upload(String name, Integer age) {
+	public String uploadFile(String name, Integer age) throws IOException {
 //		List<MultipartItem> parts=WebUtil.getMultiParts();
-//		MultipartItem part=WebUtil.getPart("file");
+		
+		System.out.println(name);
+		MultipartItem part=WebUtil.getPart("file");
+		byte[] bs=StreamUtil.readAllBytes(part.getInputStream(), true);
+		System.out.println(new String(bs,AppInfo.UTF8));
+		return "上传成功";
 	}
 	
 	
