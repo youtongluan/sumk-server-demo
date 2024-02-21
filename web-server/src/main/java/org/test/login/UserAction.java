90bccf06776b66b496e022bef2b38e02c17a1fe0
youtl <3205207767@qq.com>
2024-02-21 19:24:27 +0800

    401

--- web-server/src/main/java/org/test/login/UserAction.java
+++ web-server/src/main/java/org/test/login/UserAction.java
@@ -1,6 +1,7 @@
 package org.test.login;
 
 import org.yx.annotation.Bean;
+import org.yx.annotation.http.Web;
 import org.yx.http.WebUtil;
 import org.yx.http.user.SessionObject;
 
@@ -8,6 +9,7 @@ import org.yx.http.user.SessionObject;
 public class UserAction {
 
 	// http://localhost:8081/rest/userInfo
+	@Web(requireLogin=true)
 	public String userInfo() {
 		//这个方式可以获取到session对象。可以继承SessionObject来扩展字段
 		SessionObject session=WebUtil.getUserObject(SessionObject.class);
